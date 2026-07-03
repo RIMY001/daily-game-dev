c8db06d4a982b3a0c766e412c2658afdc83882f4
RIMY001 <2331595313@qq.com>
2026-07-03 18:59:28 +0800

    feat: 添加函数模板基础练习 day54_Template_Base.cpp；docs: 更新学习日志

---
+++ cpp/09_templates_and_generic/day54_Template_Base.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+bool myCompare(const T& a, const T& b) {
+    cout << "[通用模板] ";
+    return a > b;
+}
+
+// 具体化模板：处理 Person
+struct Person {
+    string name;
+    int age;
+};
+
+template<>
+bool myCompare<Person>(const Person& p1, const Person& p2) {
+    cout << "[具体化模板-Person] ";
+    return p1.age > p2.age;
+}
+
+int main()
+{
+    int a = 10, b = 20;
+    cout << "10 > 20 ? " << myCompare(a, b) << endl;
+
+    Person p1 = { "Zhang", 25 };
+    Person p2 = { "Li", 18 };
+    cout << "Zhang(25) older than Li(18)? " << myCompare(p1, p2) << endl;
+
+    system("pause");
+    return 0;
+}
+
+////3.模板函数外部实现
+//template<typename T>
+//class MyArray
+//{
+//public:
+//	void print(T a);
+//};
+//
+////类外实现
+//template<typename T>
+//void MyArray<T>::print(T a)
+//{
+//	cout << a << " " << "类外实现成功" << endl;
+//}
+//
+//int main()
+//{
+//	MyArray<int> arr1;
+//	arr1.print(10);
+//
+//	system("pause");
+//	return 0;
+//}
+
+////2.1通用数组容器
+//template<typename T>
+//class MyArray
+//{
+//private:
+//	T arr[100];
+//	int size = 0;//初始化
+//public:
+//	void push(T val) {
+//		if (size < 100) {
+//			arr[size++] = val;
+//		}
+//		else {
+//			// 处理溢出，例如抛出异常或扩容
+//		}
+//	}
+//	T get(int index)
+//	{
+//		return arr[index];
+//	}
+//};
+//
+//int main()
+//{
+//	//int数组
+//	MyArray<int> intArr;
+//	intArr.push(10);
+//	intArr.push(2);
+//	intArr.push(2);
+//	intArr.push(26);
+//	intArr.push(32);
+//	cout << intArr.get(2) << endl;
+//	//string数组
+//	MyArray<string> strArr;
+//	strArr.push("Hello");
+//	strArr.push("Are");
+//	strArr.push("You");
+//	strArr.push("Ok");
+//	cout << strArr.get(3) << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+
+//1.2多个参数类型
+//template<typename T1,typename T2>
+//void show(T1 a, T2 b)
+//{
+//	cout << a << " " << b << endl;
+//}
+//
+//int main()
+//{
+//	show(3.14, "zhang");
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+//1.1通用求最大值
+//template<typename T>
+//T getMax(T a, T b)
+//{
+//	return a > b ? a : b;
+//}
+//
+//int main()
+//{
+//	cout << getMax(10, 20) << endl;//T = int
+//	cout << getMax(3.14, 1.59) << endl;//T = double
+//	cout << getMax('a', 'b') << endl;//T = char
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ cpp/12_stl_and_standard_library/STL案例-评委打分.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+//创建选手类
+class Person
+{
+public:
+	string m_Name;
+	int m_Score;
+
+	Person(string n, int s) :m_Name(n), m_Score(s) {};
+};
+
+//创建具体选手
+void createPerson(vector<Person>& v)
+{
+	string nameSeed = "ABCDE";
+	for (int i = 0;i < 5;i++)
+	{
+		string name = "选手";
+		name += nameSeed[i];
+
+		int score = 0;
+		Person p(name, score);
+		//将创建的Person对象放入容器中
+		v.push_back(p);
+	}
+}
+
+//打分然后取平均值
+void setScore(vector<Person> &v)
+{
+	for (vector<Person>::iterator it = v.begin();it != v.end();it++)
+	{
+		deque<int>d;//v里每有一个对象，就创一个装打分的容器
+		for (int i = 0;i < 10;i++)
+		{
+			int score = rand() % 40 + 61;//用随机数模拟打分
+			d.push_back(score);//将打分放到容器中
+		}
+
+		//排序
+		sort(d.begin(), d.end());
+		//去除最高分和最低分
+		d.pop_back();
+		d.pop_front();
+		//取平均分
+		int sum = 0;
+		for (deque<int>::iterator it = d.begin();it != d.end();it++)
+		{
+			sum += *it;
+		}
+		int avg = sum / d.size();
+		//将平均分赋给对象的成员属性m_Score上
+		it->m_Score = avg;
+	}
+}
+
+//显示分数
+void showScore(vector<Person> &v)
+{
+	for (vector<Person>::iterator it = v.begin();it != v.end();it++)
+	{
+		cout << "姓名：" << it->m_Name << " "
+			<< "分数：" << it->m_Score << endl;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	//随机数种子
+	srand((unsigned)time(NULL));
+	//创建5名选手
+	vector<Person>v;//存放选手容器
+	createPerson(v);
+
+	//打分
+	setScore(v);
+
+	//显示分数
+	showScore(v);
+
+	system("pause");
+	return 0;
+}
