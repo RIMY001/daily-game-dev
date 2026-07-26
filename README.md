6a12457c8f74c3ff7697052063316ace6c0920ac
RIMY001 <2331595313@qq.com>
2026-07-26 16:14:07 +0800

    feat: 添加算法题「字母异位词分组」（哈希表解法），放入 algorithms/01_hash；docs: 更新学习日志

--- README.md
+++ README.md
@@ -161,3 +161,6 @@ This is a growing project,include my learning track.
 
 ### 2026-07-25
 - **完成**：1.哈希容器练习；2.算法题：两数之和；3.在Unity中创建Survival-shooter-demo项目
+
+### 2026-07-26
+- **完成**：1.STL常用算法练习；2.Lambda表达式学习；
---
+++ algorithms/01_hash/字母异位词分组.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+	vector<vector<string>> groupAnagrams(vector<string>& strs) {
+		unordered_map<string, vector<string>> mp;
+
+		for (const string& s : strs) {
+			string key = s;
+			sort(key.begin(), key.end()); // 排序后作为键
+			mp[key].emplace_back(s);	  // 原始字符归入对应组
+		}
+
+		// 收集所有分组结果
+		vector<vector<string>> result;
+		result.reserve(mp.size());
+		for (auto& [key, group] : mp) {
+			result.emplace_back(std::move(group));
+		}
+
+		return result;
+
+	}
+};
+
+// 辅助函数：格式化打印二维字符串向量
+void printResult(const vector<vector<string>>& groups) {
+	cout << "[";
+	for (size_t i = 0; i < groups.size(); ++i) {
+		cout << "[";
+		for (size_t j = 0; j < groups[i].size(); ++j) {
+			cout << "\"" << groups[i][j] << "\"";
+			if (j + 1 < groups[i].size()) cout << ", ";
+		}
+		cout << "]";
+		if (i + 1 < groups.size()) cout << ", ";
+	}
+	cout << "]" << endl;
+}
+
+int main()
+{
+	Solution sol;
+
+	// 示例 1
+	vector<string> strs1 = { "eat","tea","tan","ate","nat","bat" };
+	cout << "输入： [\"eat\",\"tea\",\"tan\",\"ate\",\"nat\",\"bat\"]" << endl;
+	cout << "输出： ";
+	printResult(sol.groupAnagrams(strs1));
+
+	// 示例 2
+	vector<string> strs2 = { "" };
+	cout << "\n输入： [\"\"]" << endl;
+	cout << "输出： ";
+	printResult(sol.groupAnagrams(strs2));
+
+	// 示例 3
+	vector<string> strs3 = { "a" };
+	cout << "\n输入： [\"a\"]" << endl;
+	cout << "输出： ";
+	printResult(sol.groupAnagrams(strs3));
+
+	system("pause");
+	return 0;
+}
